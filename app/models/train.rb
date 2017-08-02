49d8046c35bb2db381cf96dff1fefc7cce81a060
lbias <306508354@qq.com>
2017-08-02 11:50:57 +0800

    model train#available_seats

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,8 +3,15 @@ class Train < ApplicationRecord
 
   has_many :reservations
 
+  SEATS = begin
+    (1..6).to_a.map do |series|
+      ["A", "B", "C"].map do |letter|
+        "#{series}#{letter}"
+      end
+    end
+  end.flatten
+
   def available_seats
-    # todo
-    ['1A', '1B', '1C', '1D', '1F']
+    return SEATS - self.reservations.pluck(:seat_number)
   end
 end
